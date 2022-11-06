bb6b8b5e264e36b33562f460923aff8556fed478
ankit181818 <75585933+ankit181818@users.noreply.github.com>
2022-11-05 20:53:12 -0400

    added 3 unit tests for toPL and ammended previous based on my changes

--- tests/test_pig.py
+++ tests/test_pig.py
@@ -3,6 +3,24 @@ from src import pig
 
 class Tests:
     def test_engtopig(self):
-        expected = "equal"
-        actual = pig.toEnglish("equalay")
+        expected = "equal "
+        actual = pig.toEnglish(['equalay'])
+        assert actual == expected
+
+    
+    #Unit tests for toPL()
+    
+    def test_pl(self):
+        expected = "equalay "
+        actual = pig.toPL(["equal"], False)
+        assert actual == expected
+    
+    def test_int_pl(self):
+        expected = "equal-ay "
+        actual = pig.toPL(["equal"], True)
+        assert actual == expected
+
+    def test_phrase_pl(self):
+        expected = "ellohay ymay amenay isay "
+        actual = pig.toPL(["hello", "my", "name", "is"], False)
         assert actual == expected
