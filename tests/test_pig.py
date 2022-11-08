797099c1b5bc55a360e913bd13f9c57994a8a645
maazahmedd <ma5938@nyu.edu>
2022-11-08 16:13:16 -0500

    another try

--- tests/test_pig.py
+++ tests/test_pig.py
@@ -62,7 +62,7 @@ class Tests:
         assert correct_flag == True
 
     def test_speech_bubble(self):
-        expected = " ---------------------------\n(Hello, my name is Foo. I am)\n(a great person.            )\n ---------------------------"
+        expected = "\n ---------------------------\n(Hello, my name is Foo. I am)\n(a great person.            )\n ---------------------------"
         actual = pig.speech_bubble("Hello, my name is Foo. I am a great person.")
 
         assert actual == expected
